c70b02476c41fb4d6ccb6027fb852a23b3e0336b
Franz Holzinger <holzinger@ttproducts.de>
2019-02-22 00:19:04 +0100

    * compatibility for TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,33 +1,28 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = Array (
-	'title' => 'Static Info Tables (es)',
-	'description' => 'Spanish (es) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
-	'category' => 'misc',
-	'shy' => 0,
-	'version' => '6.4.4',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => 'static_countries,static_languages,static_currencies,static_territories',
-	'clearcacheonload' => 1,
-	'lockType' => '',
-	'author' => 'Franz Holzinger',
-	'author_email' => 'franz@ttproducts.de',
-	'author_company' => 'jambage.com',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array (
-		'depends' => array(
-            'typo3' => '7.6.0-8.99.99',
-            'static_info_tables' => '6.2.1-6.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Static Info Tables (es)',
+    'description' => 'Spanish (es) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+    'category' => 'misc',
+    'version' => '6.7.0',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearcacheonload' => 1,
+    'author' => 'Franz Holzinger',
+    'author_email' => 'franz@ttproducts.de',
+    'author_company' => 'jambage.com',
+    'constraints' => array (
+        'depends' => array(
+            'typo3' => '8.7.0-9.5.99',
+            'static_info_tables' => '6.7.3-6.7.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
 );
+
+
+
