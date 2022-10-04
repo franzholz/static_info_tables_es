4d28c2cf19a02637d5a6fe378404253ccf69a439
Franz Holzinger <franz.holzinger@festwein.de>
2022-10-04 18:52:58 +0200

    version of addons_em

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Static Info Tables (es)',
     'description' => 'Spanish (es) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
     'category' => 'misc',
-    'version' => '6.7.8',
+    'version' => '6.7.9',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'depends' => [
             'typo3' => '10.4.0-11.5.99',
             'static_info_tables' => '6.7.3-11.5.99',
-            'addons_em' => '0.5.0-0.5.99',
+            'addons_em' => '0.5.0-0.7.99',
         ],
         'conflicts' => [
         ],
